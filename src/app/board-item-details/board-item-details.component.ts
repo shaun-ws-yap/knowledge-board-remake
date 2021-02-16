dd00823b9ef964c59d72f37f527bf9d3498449b7
Shaun Yap <me@shaunyap.ca>
2021-02-15 18:03:25 -0800

    Route to card item details

--- src/app/board-item-details/board-item-details.component.ts
+++ src/app/board-item-details/board-item-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { items } from '../board-item/items';
 
 @Component({
   selector: 'app-board-item-details',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board-item-details.component.scss']
 })
 export class BoardItemDetailsComponent implements OnInit {
+  item;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const boardIdFromRoute = Number(routeParams.get('boardId'));
+
+    this.item = items.find(item => item.id === boardIdFromRoute);
   }
 
 }
